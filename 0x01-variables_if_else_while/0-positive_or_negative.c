7f96fabad16e1c3242afdfa4c25da5f5e8e4576f
ArfaAbdou <abdouramanarfa2@gmail.com>
2023-02-20 14:02:43 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,12 +4,15 @@
 #include <stdio.h>
 
 /*
+
 * betty style doc for function main goes there
 * main - Entry point for the program
 * This function generates a random integer between -2147483647 and 2147483647,
 * and prints whether the integer is positive, negative, or zero.
 * Return: Always 0 (success)
+
 */
+
 int main(void)
 {
 int n;

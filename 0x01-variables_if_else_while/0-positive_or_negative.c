8fdbdd047695c6d74646013dde023e061f5e3900
ArfaAbdou <111118215+ArfaAbdou@users.noreply.github.com>
2023-02-21 06:48:41 +0600

    Add files via upload

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,35 +1,23 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 #include <stdio.h>
 
-/*
-
-* betty style doc for function main goes there
-* main - Entry point for the program
-* This function generates a random integer between -2147483647 and 2147483647,
-* and prints whether the integer is positive, negative, or zero.
-* Return: Always 0 (success)
-
-*/
-
+/**
+ * main - assigns a random number to int n everytime
+ * it executes, and prints it
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-/* your code goes there */
-if (n > 0)
-{
-printf("%d is positive\n", n);
-}
-else if (n == 0)
-{
-printf("%d is zero\n", n);
-}
-else
-{
-printf("%d is negative\n", n);
-}
-return (0);
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	return (0);
 }
--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,41 +1,24 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 #include <stdio.h>
 
-/*
-*betty style doc for function main goes here
-
-*This program generates a random number using the rand function
-*and prints the last digit of the number,along with a message indicating
-*whether the last digit is greater than 5, less than 6 and not 0, or equal to 0
-*/
-
+/**
+ * main - prints the last digit of the random
+ * number stored in the variable n
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
+	int n;
 
-int n;
-
-srand(time(0));
-
-n = rand();
-
-printf("Last digit of %d is ", n);
-
-int last_digit = n % 10;
-
-if (last_digit > 5)
-{
-printf("%d and is greater than 5\n", last_digit);
-}
-else if (last_digit == 0)
-{
-printf("%d and is 0\n", last_digit);
-}
-else
-{
-printf("%d and is less than 6 and not 0\n", last_digit);
-}
-
-return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("Last digit of %d is %d ", n, n % 10);
+	if (n % 10 > 5)
+		printf("and is greater than 5\n");
+	else if (n % 10 == 0)
+		printf("and is 0\n");
+	else if (n % 10 < 6 && n % 10 != 0)
+		printf("and is less than 6 and not 0\n");
+	return (0);
 }
